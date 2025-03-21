6449ea59773b6d0ceee96a6b9c5cbff10cb45c5b
Arial1000 <lyricthefirst@gmail.com>
2025-03-20 22:45:01 -0400

    added gitignore

---
+++ server.js
@@ -0,0 +1,9 @@
+require("dotenv").config(); // Load environment variables from .env file
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => console.log("Connected to MongoDB Atlas"))
+.catch(err => console.error("MongoDB Connection Error:", err));
