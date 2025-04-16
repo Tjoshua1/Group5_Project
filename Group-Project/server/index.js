cee089c271bde41b3613e1ae742c00fdae394aa2
Arial1000 <lyricthefirst@gmail.com>
2025-04-15 20:53:35 -0400

    package.json moved

--- Group-Project/server/index.js
+++ Group-Project/server/index.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Welcom to Tap in @TU Server!" });
   });
-  
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
