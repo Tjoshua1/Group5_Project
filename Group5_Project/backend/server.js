f14932976c28ab2fc4213316fecb2bb953829d52
Arial1000 <lyricthefirst@gmail.com>
2025-05-11 11:30:06 -0400

    cors again

--- Group5_Project/backend/server.js
+++ Group5_Project/backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://tapin13.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
